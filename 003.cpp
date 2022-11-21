45bf1979d88a52c00acd5c41ccc088e45ea6f301
shirokuma89dev <code@shirokuma89.dev>
2022-11-22 01:15:14 +0900

    003

---
+++ 003.cpp
@@ -0,0 +1,24 @@
+//実際に使おう
+
+//練習問題
+//以下の関数と変数を全てwhowhoクラスにまとめてpandaインスタンスを生成してください
+
+//問題文
+int pandaFeeling = 0;
+
+void pandaHappy() {
+    pandaFeeling = pandaFeeling + 1;
+    printf("panda is happy");
+}
+
+void pandaSad() {
+    pandaFeeling = pandaFeeling - 1;
+    printf("panda is sad");
+}
+
+void pandaRestart() {
+    pandaFeeling = 0;
+    printf("panda is 人生restarted");
+}
+
+//回答は以下に記述せよ
