5ba9ba6a80a068f622a82a61d26ec689595d5e2d
shirokuma89dev <code@shirokuma89.dev>
2022-11-22 01:09:52 +0900

    002

---
+++ 002.cpp
@@ -0,0 +1,42 @@
+//文法を学ぼう
+// classの定義
+
+class CLASS_NAME {
+   public:  //とりま脳死で打つこと
+    //ここに関数や変数を書く
+    int example_variable;
+    void example_function(){
+        //関数の中身
+    };  //セミコロンいります
+};      //セミコロンを忘れない
+
+//これで型ができました。これを使ってインスタンスを作りましょう。
+
+CLASS_NAME instance_name;
+
+//インスタンス出来上がりです。
+
+//練習問題1
+// 001.cppにあったAnimalクラスからbutaインスタンスを生成してください
+
+class Animal {
+   public:
+    int x;
+
+    void migi() {
+        x = x + 1;
+    };
+
+    void hidari() {
+        x = x - 1;
+    };
+};
+
+//ここに記述せよ
+
+//練習問題2
+//なんか変数と関数を一つづつ含むクラスを適当に作ってください
+
+class /*記述*/{
+    //以下に記述せよ；
+};
